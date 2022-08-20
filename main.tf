e6fcb3628e17d1a74934047eb3ccd07fed481007
VivekVelivela <velivelavivek2000@gmail.com>
2022-08-20 13:51:46 +1000

    main update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 3.74.2"
     }
   }
 backend "remote" {
@@ -20,4 +20,5 @@ backend "remote" {
 
 provider "aws" {
   region = var.region
+  profile = "vivek-personal-iam-user"
 }
