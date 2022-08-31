8b77f884a233b80384193052b7ee8c704f4bab3c
VivekVelivela <velivelavivek2000@gmail.com>
2022-08-31 10:01:14 +1000

    changed lambda settings

--- resources.tf
+++ resources.tf
@@ -57,8 +57,12 @@ s3_key                         = aws_s3_bucket_object.file_upload_pack.key
 role                           = aws_iam_role.lambda_role.arn
 handler                        = "main.lambda_handler"
 runtime                        = "python3.8"
+memory_size                    = 10240
 source_code_hash               = filebase64sha256("${data.archive_file.zip_the_python_code.output_path}")
 depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+ephemeral_storage {
+    size = 10240 
+  }
 environment {
     variables = {
       SECRET_ARN = aws_secretsmanager_secret.spotify_credential_secret.arn
