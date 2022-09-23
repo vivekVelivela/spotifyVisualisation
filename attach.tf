a445c0c50ab8c311820d7b192a1640c5fdec76a4
VivekVelivela <velivelavivek2000@gmail.com>
2022-09-23 15:57:00 +1000

    test

--- attach.tf
+++ attach.tf
@@ -1,9 +1,17 @@
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+locals {
+  policies = {
+       one = aws_iam_policy.lambda_policy.arn,
+       two = aws_iam_policy.lambda_secret_policy.arn,
+
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {   
+    for_each = {for i, val in local.service_account_emails: i => val}
+       role        = aws_iam_role.lambda_role.name
+       policy_arn  = each.value
        depends_on = [
         aws_iam_policy.lambda_policy,aws_iam_policy.lambda_secret_policy,aws_iam_policy.lambda_secret_policy
  ]
-    for_each = toset([aws_iam_policy.lambda_policy.arn,aws_iam_policy.lambda_secret_policy.arn])
- role        = aws_iam_role.lambda_role.name
- policy_arn  = each.key
- 
 }
+
