4a859c2f36445cbeef3247f1dd7e1cfc67650bbc
VivekVelivela <velivelavivek2000@gmail.com>
2022-09-27 06:12:20 +1000

    added api gateway permission

--- permissions.tf
+++ permissions.tf
@@ -5,3 +5,23 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
   principal     = "events.amazonaws.com"
   source_arn    = "${aws_cloudwatch_event_rule.every_thousand_minutes.arn}"
 }
+
+
+# Set permissions on the lambda function, allowing API Gateway to invoke the function
+resource "aws_lambda_permission" "allow_api_gateway" {
+  # The action this permission allows is to invoke the function
+  action = "lambda:InvokeFunction"
+
+  # The name of the lambda function to attach this permission to
+  function_name = "${aws_lambda_function.extract_data_lambda_func.arn}"
+
+  # An optional identifier for the permission statement
+  statement_id = "AllowExecutionFromApiGateway"
+
+  # The item that is getting this lambda permission
+  principal = "apigateway.amazonaws.com"
+
+  # /*/*/* sets this permission for all stages, methods, and resource paths in API Gateway to the lambda
+  # function. - https://bit.ly/2NbT5V5
+  source_arn = "${aws_api_gateway_rest_api.roman-numeral-api.execution_arn}/*/*/*"
+}
