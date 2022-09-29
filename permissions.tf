ccacdc5329be0bdfbacbbaa9e498d9664fbfd961
VivekVelivela <velivelavivek2000@gmail.com>
2022-09-29 20:52:43 +1000

    test without permission

--- permissions.tf
+++ permissions.tf
@@ -8,24 +8,24 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
 
 
 # Set permissions on the lambda function, allowing API Gateway to invoke the function
-resource "aws_lambda_permission" "allow_api_gateway" {
-  # The action this permission allows is to invoke the function
-  action = "lambda:InvokeFunction"
+# resource "aws_lambda_permission" "allow_api_gateway" {
+#   # The action this permission allows is to invoke the function
+#   action = "lambda:InvokeFunction"
 
-  # The name of the lambda function to attach this permission to
-  function_name = "${aws_lambda_function.extract_data_lambda_func.function_name}"
+#   # The name of the lambda function to attach this permission to
+#   function_name = "${aws_lambda_function.extract_data_lambda_func.function_name}"
 
-  # An optional identifier for the permission statement
-  statement_id = "AllowExecutionFromApiGateway"
+#   # An optional identifier for the permission statement
+#   statement_id = "AllowExecutionFromApiGateway"
 
-  # The item that is getting this lambda permission
-  principal = "apigateway.amazonaws.com"
+#   # The item that is getting this lambda permission
+#   principal = "apigateway.amazonaws.com"
 
-  # /*/*/* sets this permission for all stages, methods, and resource paths in API Gateway to the lambda
-  # function. - https://bit.ly/2NbT5V5
-  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+#   # /*/*/* sets this permission for all stages, methods, and resource paths in API Gateway to the lambda
+#   # function. - https://bit.ly/2NbT5V5
+#   source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
 
-  depends_on = [
-    aws_api_gateway_rest_api.rest_api
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_rest_api.rest_api
+#   ]
+# }
